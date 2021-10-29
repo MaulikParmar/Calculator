6eb74c164415ea3a23884a3b12468d3540b6c92f
Maulikkumar Parmar <maulik.parmar@capgemini.com>
2021-10-29 11:48:18 +0530

    Initialize API project.

---
+++ CalculatorAPI/Calculator/Calculator.Services/Services/CalcutatorService.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services.Services
+{
+    public class CalcutatorService : ICalcutatorService
+    {
+
+    }
+}
